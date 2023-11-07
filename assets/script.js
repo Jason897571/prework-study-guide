dc6d303f47c26d6eb7f11f3c769171325faa787e
Jason <king.gs1314@outlook.com>
2023-11-07 15:17:25 -0500

    add js

---
+++ assets/script.js
@@ -0,0 +1,17 @@
+var topics = ['HTML', 'CSS', 'Git', 'JavaScript'];
+for(var x =0; x< topics.length;x++){
+    console.log(topics[x])
+}
+
+
+/* if(topics=='HTML'){
+    console.log("Let's study HTML!")
+}else if(topics=='CSS'){
+    console.log("Let's study CSS!")
+}else if(topics =='GIT'){
+    console.log("Let's study GIT!")
+}else if(topics =='JAVASCRIPT'){
+    console.log("Let's study JAVASCRIPT!")
+}else{
+    console.log("plase try again")
+} */
